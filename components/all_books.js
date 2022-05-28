beee56344a59bce850700d90497fa0f4cebb2de5
Ronald Rupp <ronald.rupp@outlook.at>
2022-05-28 23:55:42 +0200

    added more meta data to 'all_books'

--- components/all_books.js
+++ components/all_books.js
@@ -2,20 +2,26 @@ import styled from "styled-components";
 import { RichText } from "prismic-reactjs";
 import dayjs from "dayjs";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function All_Books({ data }) {
+  console.log(data);
   return (
     <Container>
       {data.fields.map((book) => (
         <Link href={book.buch._meta.uid} key={book.buch._meta.uid}>
           <a>
             <BookItem>
-              <Cover src={book.buch.cover.url} alt={book.buch.cover.alt} />
-              {/* <InfoContainer>
-            {book.buch.data.isnew && <NewIndicator>NEU</NewIndicator>}
-            {RichText.render(book.buch.data.titel)}
-            {dayjs(book.buch.data.erscheinungsdatum).format("YYYY")}
-          </InfoContainer> */}
+              <Image
+                width={400}
+                height={600}
+                src={book.buch.cover.url}
+                alt={book.buch.cover.alt}
+              />
+              <InfoContainer>
+                {RichText.render(book.buch.titel)}
+                <span>{dayjs(book.buch.erscheinungsdatum).format("YYYY")}</span>
+              </InfoContainer>
             </BookItem>
           </a>
         </Link>
@@ -42,6 +48,16 @@ const BookItem = styled.article`
   justify-content: center;
   flex-direction: column;
   align-items: center;
+  transition: transform 0.6s cubic-bezier(0.19, 1, 0.22, 1),
+    -webkit-transform 0.6s cubic-bezier(0.19, 1, 0.22, 1);
+  :hover {
+    transform: translateY(-10px);
+  }
+  @media screen and (max-width: 768px) {
+    :hover {
+      transform: translateX(10px);
+    }
+  }
 `;
 
 const Cover = styled.img`
@@ -51,10 +67,23 @@ const Cover = styled.img`
 const InfoContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  align-items: flex-start;
+  justify-content: flex-start;
+  width: 100%;
+  h1 {
+    line-height: 1;
+    margin-bottom: 0;
+    font-size: 1rem;
+    font-weight: 600;
+  }
+  span {
+    color: rgb(50, 50, 50);
+    font-size: 0.75rem;
+  }
 `;
 const NewIndicator = styled.div`
   border: 1px solid black;
-  padding: 0.5rem;
+  padding: 0.25rem;
+  border-radius: var(--border-radius);
+  font-size: 0.25rem;
 `;
