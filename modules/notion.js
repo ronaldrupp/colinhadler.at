82974d9a65959b8a2507086b3d5f8e5910eea982
Ronald Rupp <ronald.rupp@outlook.at>
2022-06-11 11:01:25 +0200

    update

--- modules/notion.js
+++ modules/notion.js
@@ -2,62 +2,67 @@ const { Client } = require("@notionhq/client");
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 const databaseId = process.env.NOTION_API_DATABASE;
 
-async function createView({ geo, page, ua}) {
-  const res = await notion.pages.create({
-    parent: {
-      database_id: databaseId,
-    },
-    properties: {
-      DateTime: {
-        date: { start: new Date() },
+async function createView({ geo, page, ua }) {
+  try {
+    const res = await notion.pages.create({
+      parent: {
+        database_id: databaseId,
       },
-      Page: {
-        rich_text: [
-          {
-            text: {
-              content: page,
+      properties: {
+        DateTime: {
+          date: { start: new Date() },
+        },
+        Page: {
+          rich_text: [
+            {
+              text: {
+                content: page,
+              },
             },
-          },
-        ],
-      },
-      Location: {
-        rich_text: [
-          {
-            text: {
-              content: `${geo.city ? geo.city : ''}, ${geo.country ? geo.country : ''}`,
+          ],
+        },
+        Location: {
+          rich_text: [
+            {
+              text: {
+                content: `${geo.city ? geo.city : ''}, ${geo.country ? geo.country : ''}`,
+              },
             },
-          },
-        ],
-      },
-      Device: {
-        rich_text: [
-          {
-            text: {
-              content: `${ua.device.vendor ? ua.device.vendor : ''}, ${ua.device.model ? ua.device.model : ''}, ${ua.device.os.name ? ua.device.os.name : ''}, ${ua.device.os.version ? ua.device.os.version : ''},`,
+          ],
+        },
+        Device: {
+          rich_text: [
+            {
+              text: {
+                content: `${ua.device.vendor ? ua.device.vendor : ''}, ${ua.device.model ? ua.device.model : ''}, ${ua.device.os.name ? ua.device.os.name : ''}, ${ua.device.os.version ? ua.device.os.version : ''},`,
+              },
             },
-          },
-        ],
-      },
-      RawUserAgentInfo: {
-        rich_text: [
-          {
-            text: {
-              content: ua
+          ],
+        },
+        RawUserAgentInfo: {
+          rich_text: [
+            {
+              text: {
+                content: ua
+              }
             }
-          }
-        ]
-      },
-      DateAndTime: {
-        title: [
-          {
-            text: {
-              content: new Date(),
+          ]
+        },
+        DateAndTime: {
+          title: [
+            {
+              text: {
+                content: new Date(),
+              },
             },
-          },
-        ],
+          ],
+        },
       },
-    },
-  });
+    });
+  }
+  catch (err) {
+    console.log(err)
+  }
   console.log(res)
 }
 
