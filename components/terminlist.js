06c0493efc88425e6692d1452cd5e9f5ad2be7cf
Ronald Rupp <ronald.rupp@outlook.at>
2024-02-23 21:03:43 +0100

    feat(termine): adjust image height and link styling

--- components/terminlist.js
+++ components/terminlist.js
@@ -47,7 +47,7 @@ export default function TerminList({ data }) {
                     : "https://images.prismic.io/colinhadler/ea91e83e-7dea-43ba-a355-b1fdd45f387c_colin_9.jpg?auto=compress,format"
                 }
                 width={500}
-                height={250}
+                height={450}
                 objectFit="cover"
               ></Image>
             </div>
@@ -203,6 +203,30 @@ const OverlayDetails = styled.div`
 
 const OverlayDescription = styled.div`
   text-align: left;
+  a {
+    font-weight: 600;
+    position: relative;
+    &:after {
+      content: "";
+      height: 2.5px;
+      width: 100%;
+      background-color: black;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      transform: scaleX(0);
+      transform-origin: right center;
+      transition: transform 0.6s cubic-bezier(0.19, 1, 0.22, 1),
+        -webkit-transform 0.6s cubic-bezier(0.19, 1, 0.22, 1);
+    }
+    &:hover {
+      &:after {
+        width: 100%;
+        transform: scaleX(1);
+        transform-origin: left center;
+      }
+    }
+  }
 `;
 const Cover = styled.img`
   width: 100%;
